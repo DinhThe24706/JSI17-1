6e475a6691881941b41b6bdbba1b7c757aab8cd3
DinhThe2407 <dinhthe24706.com>
2023-04-14 21:02:55 +0700

    first udpate

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6a6ca1e079msh37d790accc338fbp1ca924jsnb060a57eb5c8',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+export const WEATHER_API_KEY = "97516846f42897cc0b9e1eee16258437"
